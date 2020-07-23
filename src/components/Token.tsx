3934eb7bb8b036fdc4c3b1a78857f904f1e995c6
Jaime Gensler <jaimegensler0@gmail.com>
2020-07-23 12:43:53 -0700

    Add basic token displaying

--- src/components/Token.tsx
+++ src/components/Token.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Token } from 'moo';
+import { Theme } from '../types';
 
-type Props = { data: Token };
+type Props = {
+    type: string;
+    value: string;
+    theme: any;
+};
 
-export default function Token({ data }: Props) {
-    return <span>{data.text}</span>;
+export default function Token({ type, value, theme }: Props) {
+    if (type === 'NEWLINE' || !type) return null;
+
+    // This style grab solution sucks, do it better
+    const styles = theme[type.split('_')[0]] || undefined;
+    return <span style={styles}>{value}</span>;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,50 @@
 import React from 'react';
-import lexers from './lexers';
-import { Props } from './types';
-import Line from './components/Line';
 import { LexerState } from 'moo';
+
+import { Props } from './types';
+import lexers from './lexers';
+import themes from './themes';
+import Line from './components/Line';
 import Token from './components/Token';
 
 export default function CodeBlock({
     code,
     lang,
-    theme = {},
+    theme = 'oneDarkPro',
     includeLineNumbers = false,
     lineNumberStart = 1,
 }: Props) {
     const lexer = lexers[lang];
+    const colors = typeof theme === 'string' ? themes[theme] : theme;
+
+    //There may be a better way to split this to include the new line and not pass it in by force later.
     const codeLines = code.replace(/(?:^\n)|(?:\n$)/g, '').split('\n');
 
-    // This could be turned into a reduce instead of a map to remove this let
-    // and keep all the reduce state internal.
+    // This could be turned into a reduce to keep all the reduce state internal.
     let lexerState = lexer.reset().save();
     const codeComponents = codeLines.map(codeLine => {
-        const tokens = Array.from(lexer.reset(codeLine, lexerState), token => (
-            <Token data={token} key={`${token.line}-${token.col}`} />
-        ));
+        console.log(codeLine);
+        const tokens = Array.from(
+            lexer.reset(`${codeLine}\n`, lexerState),
+            token => (
+                <Token
+                    type={token.type || ''}
+                    value={token.value}
+                    theme={colors.styles}
+                    key={`${token.line}-${token.col}`}
+                />
+            ),
+        );
 
-        // NEXT LINE annotation can be implemented by checking tokens at this point.
+        // NEXT LINE annotation can be implemented here by checking tokens at this point.
         lexerState = { ...lexer.save(), line: lexerState.line + 1, col: 1 };
 
-        return <Line>{tokens}</Line>;
+        return <Line key={lexerState.line - 1}>{tokens}</Line>;
     });
 
     return (
-        <div style={{ background: 'gray' }}>
-            <pre>{codeComponents}</pre>
+        <div>
+            <pre style={{ margin: 0, ...colors.default }}>{codeComponents}</pre>
         </div>
     );
 }
