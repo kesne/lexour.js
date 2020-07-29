f24a2a9572c65a8dc0ae3e17101cf37bfc6c93b7
Jaime Gensler <jaimegensler0@gmail.com>
2020-07-29 13:37:32 -0700

    Collapse string parsing into one state

--- src/lexers/javascript.ts
+++ src/lexers/javascript.ts
@@ -1,10 +1,7 @@
 import moo from 'moo';
 
 // Strings being able to split over lines is going to take some tricks
-const STRING_escapes = {
-    STRINGESCAPE_unicode: /\\u[a-fA-F0-9]{4}/,
-    STRINGESCAPE_character: /\\./,
-};
+const STRINGESCAPE = /(?:\\u[A-Fa-f0-9]{4})|(?:\\.)/;
 
 export default moo.states({
     main: {
@@ -14,7 +11,7 @@ export default moo.states({
         },
         _PACKAGE_ANNOTATION_inline: {
             // This isn't quite working
-            match: /\/\*@.*?(?=@\*\/)@\*\//,
+            match: /\/\*@.*?@\*\//,
             value: s => s.replace(/(?:^\/\*@[ \t]*?)|(?:[ \t]*?@\*\/$)/g, ''),
         },
 
@@ -22,10 +19,9 @@ export default moo.states({
         COMMENT_singleline: /\/\/.*?$/,
 
         // Strings could probably be collapsed to one state entry with regex lookback
-        STRING_singleStart: { match: "'", push: 'stringSingle' },
-        STRING_doubleStart: { match: '"', push: 'stringDouble' },
+        STRING_start: { match: ["'", '"'], push: 'string' },
 
-        TEMPLATELITERAL_start: { match: /\`.*?\`/, push: 'templateLiteral' },
+        TEMPLATELITERAL_start: { match: '`', push: 'templateLiteral' },
 
         PUNCTUATION_leftBrace: { match: '{', push: 'main' },
         PUNCTUATION_rightBrace: { match: '}', pop: 1 },
@@ -50,10 +46,10 @@ export default moo.states({
             match: /function\*?/,
             push: 'vardecFunction',
         },
-        VARIABLE_unknown: /[_$A-Za-z][_$A-Za-z0-9]*/,
-
-        NUMBER: /[\d\.]+?/,
         BOOLISH: ['null', 'undefined', 'true', 'false'],
+
+        VARIABLE_unknown: /[_$A-Za-z][_$A-Za-z0-9]*/,
+        NUMBER: /[\d\.]+?/,
         OPERATOR: [
             // Math
             '+',
@@ -101,29 +97,25 @@ export default moo.states({
         ],
         ARROWFUNCTION: '=>',
 
+        INDENTATION: /^[ \t]+/,
         WHITESPACE: /[ \t]+/,
         NEWLINE: { match: /\n/, lineBreaks: true },
-        INDENTATION: /^[ \t]+/,
     },
     commentBlock: {
         COMMENT_block_end: { match: /\*\//, pop: 1 },
-        INDENTATION: /^[ \t]+/,
+        COMMENT_content: /.+?(?:(?=\*\/)|(?=\n))/,
         NEWLINE: { match: /\n/, lineBreaks: true },
-        COMMENT_content: /.+/,
+        INDENTATION: /^[ \t]+/,
     },
-    // Strings use greedy quantifiers, try another strategy in the future
-    stringSingle: {
-        ...STRING_escapes,
-        STRING_singleEnd: { match: "'", pop: 1 },
-        STRING_singleContent: /[^'\n]+/,
-    },
-    stringDouble: {
-        ...STRING_escapes,
-        STRING_doubleEnd: { match: '"', pop: 1 },
-        STRING_doubleContent: /[^"\n]+/,
+    // This works for now, but does create some unnecessary token breaks
+    // (but non-noticeable/problematic breaks)
+    string: {
+        STRINGESCAPE,
+        STRING_content: /(?:[^\\]+?(?="|\\))|(?:[^\\]+?(?='|\\))/,
+        STRING_end: { match: /(?:(?<=".+?)")|(?:(?<='.+?)')/, pop: 1 },
     },
     templateLiteral: {
-        ...STRING_escapes,
+        STRINGESCAPE,
         TEMP_LITERAL_end: { match: '`', pop: 1 },
         TEMP_LITERAL_interpolation_start: { match: '${', push: 'main' },
         NEWLINE: { match: /\n/, lineBreaks: true },
@@ -133,16 +125,16 @@ export default moo.states({
         WHITESPACE: /[ \t]+/,
         // This is potentially not the best strategy
         PUNCTUATION_destructuringStart: ['{', '['],
-        CONSTANT_name: { match: /[_$A-Za-z][_$A-Za-z0-9]*/, pop: 1 },
+        CONSTANT_name: { match: /[_$A-Za-z][_$A-Za-z0-9]+/, pop: 1 },
     },
     vardecVariable: {
         WHITESPACE: /[ \t]+/,
         // This is (again) potentially not the best strategy
         PUNCTUATION_destructuringStart: ['{', '['],
-        VARIABLE_name: { match: /[_$A-Za-z][_$A-Za-z0-9]*/, pop: 1 },
+        VARIABLE_name: { match: /[_$A-Za-z][_$A-Za-z0-9]+/, pop: 1 },
     },
     vardecFunction: {
         WHITESPACE: /[ \t]+/,
-        FUNCTION_name: { match: /[_$A-Za-z][_$A-Za-z0-9]*/, pop: 1 },
+        FUNCTION_name: { match: /[_$A-Za-z][_$A-Za-z0-9]+/, pop: 1 },
     },
 });
