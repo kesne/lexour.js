257b10b2b62fa5d0e4aa94e1eb8db192fe25329b
Jaime Gensler <jaimegensler0@gmail.com>
2020-08-01 18:01:26 -0700

    Force empty line rendering

--- src/components/Token.tsx
+++ src/components/Token.tsx
@@ -10,7 +10,8 @@ type Props = {
 
 export default function Token({ type, value, theme }: Props) {
     if (type === 'NEWLINE' || !type) return null;
-
+    // This is kind of a hacky solution, but it'll work for now
+    if (type === 'EMPTYLINE') return <span> </span>;
     const primaryType = type.replace(/(?<=^_?[A-Z]+)_.+/, '');
     const styles = theme[primaryType] || undefined;
     return <span style={styles}>{value}</span>;
